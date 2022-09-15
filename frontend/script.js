7e29fb5b49d75fac691378f5e5539ffe1ef6c7e5
Jancsurák Bence <jbence357@gmail.com>
2022-09-15 18:30:38 +0200

    ciklusok

---
+++ frontend/script.js
@@ -0,0 +1,16 @@
+let value = true
+console.log(typeof(value))
+value = String(value)
+console.log(typeof(value))
+console.log(6/2)
+let str ="464"
+console.log(typeof str)
+let num = Number(str)
+console.log(typeof num)
+num = num +1,2
+console.log(num)
+console.log(typeof num)
+console.log(Boolean(1))
+console.log(Boolean("trash"))
+console.log(Boolean(""))
+console.log(Boolean(" "))
---
+++ frontend/script2.js
@@ -0,0 +1,16 @@
+//let i = 3
+//while(i){
+ //}
+"use strict"
+/*for(let i= 1;i<3;i++){
+    console.log(i)
+    document.write("<br>hello</br>")
+}
+i = 2;
+console.log(i)
+*/
+let j
+for(j = 0;j<3;j++){
+    console.log(j)
+}
+console.log(j)
